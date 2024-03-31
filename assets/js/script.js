cc13d8800276ffde05863034a673b6e05b8e4b4f
Daniela Leal <falfada@gmail.com>
2024-03-31 14:11:19 +1100

    Created modal, datepicker and event listener to add a new card

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,45 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
-}
+function generateTaskId() {}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+  const taskTitle = $("#taskTitle").val();
+  const taskDueDate = $("#taskDueDate").val();
+  const taskDescription = $("#taskDescription").val();
+  const taskInfo = {
+    tasktTitle: taskTitle,
+    taskDueDate: taskDueDate,
+    taskDescription: taskDescription,
+  };
 
+  console.log(taskInfo);
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  // Render task list
+  renderTaskList();
 
+  // Event Listener
+  $("#newTaskButton").on("click", createTaskCard);
+
+  // Due Date datepicker
+  $("#taskDueDate").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
 });
