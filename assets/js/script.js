8ebebc75922fc05459a9b78d180b6d1485a109e3
Daniela Leal <falfada@gmail.com>
2024-04-01 12:10:45 +1100

    Created function to render cards when loading page and function to create card

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,87 @@ function generateTaskId() {}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-  const taskTitle = $("#taskTitle");
-  const taskDueDate = $("#taskDueDate");
-  const taskDescription = $("#taskDescription");
-  const taskInfo = {
-    tasktTitle: taskTitle.val(),
-    taskDueDate: taskDueDate.val(),
-    taskDescription: taskDescription.val(),
-  };
-  taskTitle.val("");
-  taskDueDate.val("");
-  taskDescription.val("");
-  
+  console.log(task);
+  const taskCard = $(
+    `<div class="card draggable my-3" data-project-id="${task.id}"></div>`
+  );
+  const taskHeader = $(
+    `<div class="card-header"><h4 class="card-title">${task.title}</h4></div>`
+  );
+  const taskBody = $(`<div class="card-body"></div>`);
+  const taskDescription = $(`<p class="card-text">${task.description}</p>`);
+  const taskDueDate = $(`<p class="card-text">${task.dueDate}</p>`);
+  const taskButton = $(
+    `<button class="btn btn-danger delete" data-project-id="${task.id}">Delete</button>`
+  );
+
+const now = dayjs();
+    if(now.isSame(task.dueDate, 'day')){
+      taskCard.addClass('bg-warning text-white');
+    } else if(now.isAfter(task.dueDate, 'day')){
+      taskCard.addClass('bg-danger text-white');
+      taskButton.addClass('border-light');
+    }
+
+  taskCard.append(taskHeader);
+  taskCard.append(taskBody);
+  taskBody.append(taskDescription);
+  taskBody.append(taskDueDate);
+  taskBody.append(taskButton);
+  $('#todo-cards').append(taskCard);
+  // return taskCard;
+
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {}
+function renderTaskList() {
+    if(!taskList){
+      return [];
+    } else{
+      for (const task of taskList) {
+        createTaskCard(task);
+      }
+    }
+}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  event.preventDefault();
+
+  const taskTitle = $("#taskTitle");
+  const taskDueDate = $("#taskDueDate");
+  const taskDescription = $("#taskDescription");
+
+  const newTask = {
+    // ? Here we use a tool called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.
+    id: crypto.randomUUID(),
+    title: taskTitle.val(),
+    description: taskDescription.val(),
+    dueDate: taskDueDate.val(),
+  };
+  
+  let tasks;
+  if(!taskList){
+    tasks = [];
+  } else{
+    tasks = taskList;
+  }
+  
+  tasks.push(newTask);
+  
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  taskTitle.val('');
+  taskDueDate.val('');
+  taskDescription.val('');
+
+  $('#formModal').modal('hide');
+  
+  createTaskCard(newTask);
+
+  
+
+}
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {}
@@ -39,7 +101,8 @@ $(document).ready(function () {
   renderTaskList();
 
   // Event Listener
-  $("#newTaskButton").on("click", createTaskCard);
+  $("#newTaskButton").on("click", handleAddTask);
+  // Add event listener to handleDeleteTask
 
   // Due Date datepicker
   $("#taskDueDate").datepicker({
