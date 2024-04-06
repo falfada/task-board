d5e5e98a1d6c0c03691b4a1f6d0fb68e972c9505
Daniela Leal <falfada@gmail.com>
2024-04-07 08:06:50 +1000

    Created generateTaskId function and event listener to deleter card container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,14 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+
+function generateTaskId() {
+  let id = new Date().getTime();
+  return id;
+}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-  console.log(task);
   const taskCard = $(
     `<div class="card draggable my-3" data-project-id="${task.id}"></div>`
   );
@@ -60,7 +63,7 @@ function handleAddTask(event) {
 
   const newTask = {
     // ? Here we use a tool called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.
-    id: crypto.randomUUID(),
+    id: generateTaskId(),
     title: taskTitle.val(),
     description: taskDescription.val(),
     dueDate: taskDueDate.val(),
@@ -90,7 +93,10 @@ function handleAddTask(event) {
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {}
+function handleDeleteTask(event) {
+  const cardContainer = event.target.parentElement.parentElement;
+  console.log(parent);
+}
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {}
@@ -103,6 +109,7 @@ $(document).ready(function () {
   // Event Listener
   $("#newTaskButton").on("click", handleAddTask);
   // Add event listener to handleDeleteTask
+  $(".delete").on("click", handleDeleteTask);
 
   // Due Date datepicker
   $("#taskDueDate").datepicker({
